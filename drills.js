93951ba5070126b77428edcf300487ec72c895be
Sherry McCarty <sherrydmccarty@gmail.com>
2020-04-21 17:00:05 -0400

    Smaug reports to noone

--- drills.js
+++ drills.js
@@ -61,3 +61,23 @@ let peopleObjects = [
 ];
 
 peopleObjects.forEach((person) => console.log(`${person.name}: ${person.jobTitle}`));
+
+//add boss to everyone except the actual owner
+//change interation to print: "${title} ${name} reports to ${boss}."
+// if no boss, "${title} ${name} doesn't report to anybody."
+
+let peopleObjects2 = [
+  {name: 'dude', jobTitle: 'awesome', boss: 'Smaug'},
+  {name: 'dudette', jobTitle: 'awesomer', boss: 'Smaug'},
+  {name: 'Thomas', jobTitle: 'none', boss: 'Smaug'},
+  {name: 'Sherry', jobTitle: 'none', boss: 'Smaug'},
+  {name: 'Smaug', jobTitle: 'dragon'}
+];
+
+peopleObjects2.forEach((person) => {
+  if(person.hasOwnProperty('boss')) {
+  console.log(`${person.jobTitle} ${person.name} reports to ${person.boss}.`);
+  } else {
+    console.log(`${person.jobTitle} ${person.name} doesn't report to anybody.`);
+  }
+});
